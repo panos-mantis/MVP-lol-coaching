6a519bf4267fcede2d5f2757d8ea027748d52a02
panos mantis <png.mantis@gmail.com>
2023-06-28 22:32:34 +0300

    Second commit with some Hero component

--- Frontend/lol-coaching/src/App.js
+++ Frontend/lol-coaching/src/App.js
@@ -1,12 +1,18 @@
 import Navbar from "./components/Navbar/Navbar";
-import { BrowserRouter } from "react-router-dom";
-
+import { BrowserRouter , Routes ,Route} from "react-router-dom";
+import Hero from "./components/Hero/Hero";
 function App() {
   return (
     <div className="App">
       <Navbar/>
-
-      
+      <Hero/>
+      {/* <Routes>
+        <Route to="/" element={<Home/>}/>
+        <Route to="/about" element={<About/>}/>
+        <Route to="/coaching" element={<Coaching/>}/>
+        <Route to="/sign-in" element={<SignIn/>}/>
+        <Route to="/sign-up" element={<SignUp/>}/>
+      </Routes> */}
     </div>
   );
 }
